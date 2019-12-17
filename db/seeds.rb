968cdd1a3d20d5f910656dcd33a6b8326b15de98
Anja Laube <mail@anjalaube.com>
2019-12-17 16:53:29 +0100

    New seeds 5 Cocktails plus multiple ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,41 @@
 puts "Delete DB...."
 Ingredient.destroy_all
 
-puts "Creating new records..."
+puts "Creating ingreditens..."
+Ingredient.create(name: "Rum")
+Ingredient.create(name: "Soda")
+Ingredient.create(name: "Limes")
+Ingredient.create(name: "Brown sugar")
+Ingredient.create(name: "Mint")
+
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Tomato juice")
+Ingredient.create(name: "Lemon juice")
+Ingredient.create(name: "Salt")
+Ingredient.create(name: "Pepper")
+Ingredient.create(name: "Tabasco")
+Ingredient.create(name: "Worcester sauce")
+
+Ingredient.create(name: "Kahlua")
+Ingredient.create(name: "Whole milk")
+
+Ingredient.create(name: "Lime juice")
+Ingredient.create(name: "Simple syrup")
+
+Ingredient.create(name: "Whiskey")
+Ingredient.create(name: "Egg white")
+
+Ingredient.create(name: "orange")
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "brown sugar")
 Ingredient.create(name: "basil")
 Ingredient.create(name: "ginger")
 Ingredient.create(name: "orange")
 
 
-puts "Create cocktail..."
+puts "Createing cocktails..."
 Cocktail.create(name: "Mojito")
 Cocktail.create(name: "Bloody Mary")
 Cocktail.create(name: "White Russian")
 Cocktail.create(name: "Daquiri")
+Cocktail.create(name: "Whiskey Sour")
